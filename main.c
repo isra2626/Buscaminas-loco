f31c70ce8c0ff5e3cb7532a415ba39890d45f973
isra2626 <iayaladiaz26@gmail.com>
2022-05-27 09:26:39 -0500

    Practicando matrices

--- main.c
+++ main.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-void saluditos();
+void introduce(int matriz[][5]);
+void leer(int matriz[][5]);
 int main()
-{
-    saluditos();
-    return 0;
+{   int matriz[5][5];
+    introduce(matriz);
+    leer(matriz);
+    srand (getpid());
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            matriz[i][j]= random()%2;
+        }
+        
+    }
+    
 }
 
-void saluditos(){
-    printf("Hello world!\n");
-    printf("prueba 222");
+
+void leer(int matriz[5][5]){
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+           printf("%i ",matriz[i][j]);
+        }
+        printf("\n");
+    }
+    
 }

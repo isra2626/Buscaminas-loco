5c79409e3190935e263848ca56f83e5ec93a5949
isra2626 <iayaladiaz26@gmail.com>
2022-05-27 18:33:13 -0500

    prueba

--- main.c
+++ main.c
@@ -5,30 +5,33 @@
 void introduce(int matriz[][5]);
 void leer(int matriz[][5]);
 int main()
-{   int matriz[5][5];
+{   int matriz[7][5];
     introduce(matriz);
     leer(matriz);
-    srand (getpid());
-    for (int i = 0; i < 5; i++)
-    {
-        for (int j = 0; j < 5; j++)
-        {
-            matriz[i][j]= random()%2;
-        }
-        
-    }
-    
+    printf("\n\n");
+    introduce(matriz);
+    leer(matriz);
+    return 0;
 }
 
-
-void leer(int matriz[5][5]){
-    for (int i = 0; i < 5; i++)
+void leer(int matriz[][5]){
+    for (int i = 0; i < 7; i++)
     {
         for (int j = 0; j < 5; j++)
         {
            printf("%i ",matriz[i][j]);
         }
         printf("\n");
+    }   
+}
+
+void introduce(int matriz [][5]){
+    srand (getpid());
+    for (int i = 0; i < 7; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            matriz[i][j]= random()%2;
+        }      
     }
-    
 }

afe7ee68f0fc0f86224ad79a9f09e00c5393396b
Israel Antonio Ayala Diaz <83836098+isra2626@users.noreply.github.com>
2022-06-01 19:36:20 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,23 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
 
-void introduce(int matriz[][5]);
-void leer(int matriz[][5]);
+#define FILAS 10
+#define COLUMNAS 10
+#define CANTIDAD_DE_MINAS 5
+
+void colocador_de_minas();
+void mete_la_mina();
+void leer_matrices();
+void introduce_cero();
+int aleatorio_en_rango();
+void minas_cercanas_1();
+
 int main()
-{   int matriz[7][5];
-    introduce(matriz);
-    leer(matriz);
-    printf("\n\n");
-    introduce(matriz);
-    leer(matriz);
+{  int matriz_minas[FILAS][COLUMNAS];
+    int matriz_general[FILAS][COLUMNAS];
+    srand(time(NULL));
+    introduce_cero(matriz_minas);
+    introduce_cero(matriz_general);
+    colocador_de_minas(matriz_minas);
+    leer_matrices(matriz_minas);
+    printf("\n\n\n\n\n");
+    minas_cercanas_1(matriz_minas,matriz_general);
+    leer_matrices(matriz_general);
+    printf("\n\n\n\n\n");
+    leer_matrices(matriz_minas);
     return 0;
 }
+int aleatorioEnRango(int minimo, int maximo){
+    
+   return minimo + rand() / (RAND_MAX / (maximo - minimo + 1) + 1);
+}
 
-void leer(int matriz[][5]){
-    for (int i = 0; i < 7; i++)
+void mete_la_mina(int fila, int columna, int matriz[][COLUMNAS]){
+    matriz[fila][columna]=1;
+}
+
+void colocador_de_minas (int matriz[FILAS][COLUMNAS]){
+    int l;
+  for (l = 0; l < CANTIDAD_DE_MINAS; l++) {
+    int fila = aleatorioEnRango(0, FILAS - 1);
+    int columna = aleatorioEnRango(0, COLUMNAS - 1);
+    if(matriz[fila][columna]==0){
+        mete_la_mina(fila, columna, matriz);
+        }else{
+            l=l-1;
+            continue;
+        }
+    
+  }
+} 
+
+void leer_matrices(int matriz[][COLUMNAS]){
+    for (int i = 0; i < FILAS; i++)
     {
-        for (int j = 0; j < 5; j++)
+        for (int j = 0; j < COLUMNAS; j++)
         {
            printf("%i ",matriz[i][j]);
         }
@@ -25,13 +64,25 @@ void leer(int matriz[][5]){
     }   
 }
 
-void introduce(int matriz [][5]){
-    srand (getpid());
-    for (int i = 0; i < 7; i++)
+void introduce_cero(int matriz [][COLUMNAS]){
+    for (int i = 0; i < FILAS; i++)
     {
-        for (int j = 0; j < 5; j++)
+        for (int j = 0; j < COLUMNAS; j++)
         {
-            matriz[i][j]= random()%2;
+            matriz[i][j]= 0;
         }      
     }
+}
+
+void minas_cercanas_1(int matriz_a_calcular[][COLUMNAS], int matriz_a_devolver[FILAS][COLUMNAS]){
+
+    for (int i = 1; i < FILAS-1; i++)
+    {
+        for (int j = 1; j < COLUMNAS-1; j++)
+        {
+            matriz_a_calcular[i][j]=8;
+        }
+        
+    }
+    
 }

4e3e419123e43097b27cc0231938d569cadc6809
isra2626 <iayaladiaz26@gmail.com>
2022-06-02 11:08:00 -0500

    arreglando recorrido de matriz

---
+++ pruebas.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void recorrer_matriz_de_forma_cuadrada();
+void leer_matrices();
+void introduce_cero();
+
+int main(){
+    int matriz_de_prueba[10][10];
+    introduce_cero(matriz_de_prueba);
+    recorrer_matriz_de_forma_cuadrada(matriz_de_prueba);
+    leer_matrices(matriz_de_prueba);
+    return 0;
+}
+
+void leer_matrices(int matriz[][10]){
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+           printf("%i ",matriz[i][j]);
+        }
+        printf("\n");
+    }   
+}
+
+void introduce_cero(int matriz [][10]){
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            matriz[i][j]= 0;
+        }      
+    }
+}
